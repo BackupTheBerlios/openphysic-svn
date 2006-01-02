56833c2a2583c30309dc79c46f97a28f92c3cc18
scls19fr <scls19fr@1499e563-6509-0410-bd2e-facac817758d>
2006-01-02 20:37:58 +0000

    svn import de fieldEB en Java

---
+++ fieldeb_java/trunk/CGrid.java
@@ -0,0 +1,197 @@
+/**************************************************************************
+ *   Copyright (C) 2005 by Sebastien CELLES                                *
+ *   s.cls@laposte.net                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+/*
+ * Grille
+ */
+public class CGrid
+{
+    private double m_x_min;
+    private double m_x_max;
+    private double m_x_space;
+
+    private double m_y_min;
+    private double m_y_max;
+    private double m_y_space;
+
+    private double m_z_min;
+    private double m_z_max;
+    private double m_z_space;
+
+    private boolean m_visible;
+
+
+    CGrid(double x_min, double x_max, double x_space,
+	  double y_min, double y_max, double y_space,
+	  double z_min, double z_max, double z_space,
+	  boolean visible )
+    {
+	set_x_min(x_min);
+	set_x_max(x_max);
+	set_x_space(x_space);
+
+	set_y_min(y_min);
+	set_y_max(y_max);
+	set_y_space(y_space);
+
+	set_z_min(z_min);
+	set_z_max(z_max);
+	set_z_space(z_space);
+
+	set_visible(visible);
+    }
+
+
+    double get_x_min()
+    {
+	return m_x_min;
+    }
+
+    void set_x_min(double value)
+    {
+	m_x_min = value;
+    }
+
+
+
+    double get_x_max()
+    {
+	return m_x_max;
+    }
+
+    void set_x_max(double value)
+    {
+	m_x_max = value;
+    }
+
+
+
+    double get_x_space()
+    {
+	return m_x_space;
+    }
+
+    void set_x_space(double value)
+    {
+	m_x_space = value;
+    }
+
+
+
+    double get_y_min()
+    {
+	return m_y_min;
+    }
+
+    void set_y_min(double value)
+    {
+	m_y_min = value;
+    }
+
+
+
+    double get_y_max()
+    {
+	return m_y_max;
+    }
+
+    void set_y_max(double value)
+    {
+	m_y_max = value;
+    }
+
+
+
+    double get_y_space()
+    {
+	return m_y_space;
+    }
+
+    void set_y_space(double value)
+    {
+	m_y_space = value;
+    }
+
+
+
+    double get_z_min()
+    {
+	return m_z_min;
+    }
+
+    void set_z_min(double value)
+    {
+	m_z_min = value;
+    }
+
+
+
+    double get_z_max()
+    {
+	return m_z_max;
+    }
+
+    void set_z_max(double value)
+    {
+	m_z_max = value;
+    }
+
+
+
+    double get_z_space() 
+    {
+	return m_z_space;
+    }
+
+    void set_z_space(double value)
+    {
+	m_z_space = value;
+    }
+
+
+
+    boolean Is_visible()
+    {
+	return m_visible;
+    }
+
+    boolean get_visible()
+    {
+	return m_visible;
+    }
+
+    void set_visible(boolean value)
+    {
+	m_visible = value;
+    }
+
+    void set_visible()
+    {
+	m_visible = true;
+    }
+
+    void unset_visible()
+    {
+	m_visible = false;
+    }
+
+
+}
---
+++ fieldeb_java/trunk/CPosition.java
@@ -0,0 +1,71 @@
+/**************************************************************************
+ *   Copyright (C) 2005 by Sebastien CELLES                                *
+ *   s.cls@laposte.net                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*
+ * Position d'un point
+ */
+public class CPosition
+{
+    private double m_x;
+    private double m_y;
+    private double m_z;
+
+    CPosition (double x, double y, double z)
+    {
+	m_x = x;
+	m_y = y;
+	m_z = z;
+    }
+
+
+
+    void set_x(double x)
+    {
+	m_x = x;
+    }
+
+    void set_y(double y)
+    {
+	m_y = y;
+    }
+
+    void set_z(float z)
+    {
+	m_z = z;
+    }
+
+
+
+    double get_x()
+    {
+	return m_x;
+    }
+
+    double get_y()
+    {
+	return m_y;
+    }
+
+    double get_z()
+    {
+	return m_z;
+    }
+
+}
