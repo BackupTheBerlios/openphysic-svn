bac1703666a8b28cb1621d921abab045c828a116
scls19fr <scls19fr@1499e563-6509-0410-bd2e-facac817758d>
2008-02-28 17:36:25 +0000

    made a copy

---
+++ uc/atmel_avr/servo-m8-uart-pwm/README.txt
@@ -0,0 +1,35 @@
+Ce projet décrit le fonctionnement d'un testeur de servo de modélisme.
+Il permet de commander un servomoteur à l'aide d'une tension continue (A2D_MODE) ou à l'aide d'une liaison série (UART_MODE) et génère un signal compatible avec les servomoteurs utilisés en modélisme c'est à dire un signal périodique de 20 ms et de largeur d'impulsion variable entre 1 et 2 ms.
+Nécessite :
+- WinAVR-gcc
+- AVRLib
+
+Rôles des différents cavaliers (JUMPER)
+JP2_MODE 0=A2D_MODE 1=UART_MODE
+JP2_INPUT
+ Entrée analogique sur borne Vin_ext (utilisation d'un générateur continu ajustable externe entre 0 et 5V) 
+ Potentiomètre
+JP VCC / JP MID / JP GND
+ Cavaliers permettant d'activer une résistance de pull-up afin que la borne ADC0 ne soit pas "en l'air" si le fil ammenant la tension Vin_ext est déconnecté.
+
+
+ToDo:
+ Changer de uC car le code dépasse 8ko !
+ Changer le port sur lequel est relié le cavalier mode (à cause de l'ISP)
+
+
+Compiler le programme
+$ cd repertoire_projet
+$ make
+
+Effacer l'ATmega8
+$ avrdude -p m8 -c stk500v2 -P COM4 -e
+
+Programmer l'ATMega8
+$ cd repertoire_projet
+$ avrdude -p m8 -c stk500v2 -P COM4 -U flash:w:main.hex
+
+Passer l'oscillateur interne de l'ATmega8 de 1MHz à 8Mhz
+$ avrdude -p m8 -c stk500v2 -P COM4 -t
+avrdude> r lfuse
+avrdude> w lfuse 0 0xC4
---
+++ uc/atmel_avr/servo-m8-uart-pwm/main.c
@@ -0,0 +1,139 @@
+#include <avr/io.h>
+
+#define PERIOD 20000
+#define MIN_WIDTH 1000
+#define MAX_WIDTH 2000
+#define MED_WIDTH 1500
+
+#include "global.h"		// include our global settings
+#include "a2d.h"		// include A/D converter function library
+#include "uart.h"		// include uart function library
+#include "rprintf.h"	// include printf function library
+//#include "timer.h"		// include timer function library (timing, PWM, etc)
+//#include "vt100.h"		// include VT100 terminal library
+
+
+enum mode { A2D_MODE = 0, UART_MODE};
+//#define A2D_MODE 0
+//#define UART_MODE 1
+
+volatile uint8_t mode;
+
+void init_uart(void) {
+	// initialize our libraries
+	// initialize the UART (serial port)
+	uartInit();
+	// set the baud rate of the UART for our debug/reporting output
+	uartSetBaudRate(9600);
+	// set uartSendByte as the output for all rprintf statements
+	rprintfInit(uartSendByte);
+	// initialize the timer system
+	timerInit();
+	// initialize vt100 library
+	vt100Init();
+	vt100ClearScreen();
+	// print a little intro message so we know things are working
+	//rprintf("Servo tester-S.CELLES\r\n");
+	//rprintf("Sebastien CELLES\r\n");
+	//rprintf("IUT de Poitiers\r\n");
+	//rprintf("Genie Thermique et Energie\r\n");
+}
+
+void init_output(void) {
+   // OC1A as output
+   DDRB |= (1<<PB1);
+   // TOP, set for 50Hz (20ms)
+   ICR1 = PERIOD;
+   // Center outputs (1.5ms)
+	//OCR1A = MIN_WIDTH;
+   OCR1A = MED_WIDTH;
+   //OCR1A = MAX_WIDTH;
+   // Timer 1 fast PWM mode 14
+   // Clear on compare, set at TOP
+   TCCR1A = (1<<COM1A1)|(1<<WGM11);
+   //TCCR1B = (1<<WGM13)|(1<<WGM12)|(0<<CS12)|(0<<CS11)|(1<<CS10); // clk=1Mhz - no prescaler
+   TCCR1B = (1<<WGM13)|(1<<WGM12)|(0<<CS12)|(1<<CS11)|(0<<CS10); // clk=8Mhz - prescaler=8
+
+}
+
+//unsigned char pos; // 8 bits
+volatile int pos8; // 8 bits
+volatile unsigned short pos; // 16 bits
+
+int main(void)
+{
+	/* Voyant ON */
+	DDRD |= (1<<DDD7);
+	PORTD |= (1<<PD7);
+
+	/* Voyant POWER */
+	DDRB |= (1<<DDB3);
+	PORTB |= (1<<PB3);
+	TCCR2 = (0<<FOC2)|(1<<WGM20)|(1<<COM21)|(0<<COM20)|(1<<WGM21)|(0<<CS22)|(0<<CS21)|(1<<CS20);
+    OCR2=0;
+
+
+	init_uart();
+	init_output();
+	
+	/* Mode 0=A2D 1=UART */
+	//mode=A2D_MODE;
+	DDRB &= ~(1<<PB0); // digital input for mode 
+
+	a2dInit(); // init analog to digital converter
+	
+	
+	while (1)
+      {
+	  		mode=PINB & (1<<PB0); /* get mode A2D=0 or UART=1*/
+
+			if (mode == UART_MODE) {
+				// 8bits
+				pos8=-1;
+				while(pos8==-1 && mode==UART_MODE) {
+					pos8=uartGetByte();
+			  		mode=PINB & (1<<PB0); /* get mode A2D=0 or UART=1*/
+				}
+				OCR1A=((((double) pos8)*1000.0)/255.0) + MIN_WIDTH;
+				OCR2=pos8; /* voyant POWER */
+			}
+			else { //if (mode == A2D_MODE) {
+				//pos=a2dConvert8bit(0);
+				//pos=a2dConvert8bit(0);
+				pos=a2dConvert10bit(0);
+				//OCR1A=(pos<<2) + MIN_WIDTH; // 1000 approx 1024
+				//OCR1A=((((double) pos)*1000.0)/255.0) + MIN_WIDTH;
+				OCR1A=((((double) pos)*1000.0)/1023.0) + MIN_WIDTH; /* */
+				OCR2=pos>>2; /* voyant POWER */
+			}
+
+			//rprintf("Testeur de servo en cours - Sebastien CELLES - Mode=%d - Throttle=%d\r\n",mode, OCR2);
+			rprintf("Mode=%d-Throttle=%d\r\n",mode, OCR2);
+
+      };
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
